c011d1b226c1fae5cdbd5a75e538d0e29c679c62
benedictNg <benedictng1234@gmail.com>
2024-12-27 16:37:15 +0800

    feat: Delete old goBackend

--- internal/db/migrations/000001_create_users_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username TEXT UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL,
-    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
-    user_public_id char(21) UNIQUE DEFAULT nanoid(21)
-  );
--- internal/db/migrations/000002_create_posts_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS POSTS (
-    id SERIAL PRIMARY KEY,
-    title TEXT UNIQUE NOT NULL,
-    content TEXT NOT NULL,
-    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
-    user_public_id char(21) UNIQUE DEFAULT nanoid(21),
-
-    FOREIGN KEY (user_public_id) REFERENCES users(user_public_id) 
-  );
