b8adb1d95efb089e043c6adfc85607056c730261
benedictNg <benedictng1234@gmail.com>
2025-01-10 15:07:53 +0800

    feat: Add default parentCategories.sql to be used to create default categories

---
+++ goBackend/internal/db/utils/parentCategory.sql
@@ -0,0 +1,6 @@
+-- default parent categories - created by user 1 'admin'
+INSERT INTO categories (id, public_id, name, description, parent_id, created_by)
+VALUES
+    (2, 'hQW0H2q0nXXKDoXvatxwm', 'Academics', 'Discuss about school!', 'PARENT', 1),
+    (3, 'TOTaswlgbOj2MEYKHeckk', 'Hobbies', 'Discuss about hobbies!', 'PARENT', 1),
+    (4, 'V8a4HFZXGm-KUTvi71lPi', 'CCA', 'Discuss about CCAs!', 'PARENT', 1);
